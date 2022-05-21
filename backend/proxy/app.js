2490db9badb8a97f54dc8a0ffd61da22bd8bea7f
Bornord <best.enguerran@gmail.com>
2022-05-21 16:58:00 +0200

    test

--- backend/proxy/app.js
+++ backend/proxy/app.js
@@ -1,29 +1,27 @@
 const express = require('express');
-const axios = require("axios");
+const axios = require('axios');
 const app = express();
 
 app.use(express.json());
 api = axios.create({
 	baseURL: 'http://localhost:3500',
 });
-const Post = async (path,data,success,error)=>{
-	try{
-		const res = await api.post("/Server?op="+path,data)
-		success(res)		
-	}catch(e){
-		error(e)
+const Post = async (path, data, success, error) => {
+	try {
+		const res = await api.post('/Server?op=' + path, data);
+		success(res);
+	} catch (e) {
+		error(e);
 	}
-}
-const Get = async (path,success,error)=>{
-	try{
-		const res = await api.get("/Server?op="+path)
-		if(success!=null)
-			success(res)		
-	}catch(e){
-		if(error!=null)
-			error(e)
+};
+const Get = async (path, success, error) => {
+	try {
+		const res = await api.get('/Server?op=' + path);
+		if (success != null) success(res);
+	} catch (e) {
+		if (error != null) error(e);
 	}
-}
+};
 /* 
 Déclaration des permissions CORS
 - Cela inclut l'origine des utilisateurs qui peuvent faire des requêtes.
@@ -48,40 +46,43 @@ Fonction principale qui conditionne les appels et leur traitement dans l'environ
 app.route('/Proxy')
 	.get((req, res, next) => {
 		console.log(req.query);
-		if(req.query.op != null){
+		if (req.query.op != null) {
 			console.log(req.query.op);
-			Get(req.query.op,
-				(data)=>{
+			Get(
+				req.query.op,
+				(data) => {
 					res.status(201).json(data);
 				},
-				()=>{
+				() => {
 					res.status(404).json({ msg: 'error on server' });
 				}
-				)
-		}else{
+			);
+		} else {
 			res.status(404).json({ msg: 'error on proxy' });
 		}
+		next();
 	})
 	.post((req, res, next) => {
 		console.log(req.query);
-		if(req.query.op != null){
-			Post(req.query.op,
-				(data)=>{
+		if (req.query.op != null) {
+			Post(
+				req.query.op,
+				(data) => {
 					res.status(201).json(data);
 				},
-				()=>{
+				() => {
 					res.status(404).json({ msg: 'error' });
 				}
-				)
-		}else{
+			);
+		} else {
 			res.status(404).json({ msg: 'error' });
 		}
-	})
+		next();
+	});
 
 app.use('/', (req, res, next) => {
 	console.log('Bien reçu, bien reçu.');
 	res.status(201).json({ msg: 'hello world back' });
 });
-
 
 module.exports = app;
