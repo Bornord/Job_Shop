9a6d765601313be22c1e3135454a8fa4f50a1187
unknown <darwil.nicolas@gmail.com>
2022-05-21 12:49:38 +0200

    add proxy

--- frontend/job_shop/src/logic/api/api.jsx
+++ frontend/job_shop/src/logic/api/api.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const post = async (path,data,success,error)=>{
 	try{
-		const res = await api.post("/Server?op="+path,data)
+		const res = await api.post("/Proxy?op="+path,data)
 		success(res)		
 	}catch(e){
 		error(e)
@@ -10,7 +10,7 @@ export const post = async (path,data,success,error)=>{
 }
 export const get = async (path,success,error)=>{
 	try{
-		const res = await api.get("/Server?op="+path)
+		const res = await api.get("/Proxy?op="+path)
 		if(success!=null)
 			success(res)		
 	}catch(e){
@@ -20,6 +20,6 @@ export const get = async (path,success,error)=>{
 }
 
 export default api = axios.create({
-	baseURL: 'http://localhost:3500',
+	baseURL: 'http://localhost:8000',
 });
 
