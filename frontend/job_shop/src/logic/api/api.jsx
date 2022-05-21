c465a4f578c0f3b6e230e3a7a6b6b944ad7cbd97
Bornord <best.enguerran@gmail.com>
2022-05-21 16:53:48 +0200

    delete call

--- frontend/job_shop/src/logic/api/api.jsx
+++ frontend/job_shop/src/logic/api/api.jsx
@@ -1,28 +1,22 @@
 import axios from 'axios';
 
-
 export const api = axios.create({
 	baseURL: 'http://localhost:8000',
 });
 
-export const post = async (path,data,success,error)=>{
-	try{
-		const res = await api.post("/Proxy?op="+path,data)
-		success(res)		
-	}catch(e){
-		error(e)
+export const post = async (path, data, success, error) => {
+	try {
+		const res = await api.post('/Proxy?op=' + path, data);
+		success(res);
+	} catch (e) {
+		error(e);
 	}
-}
-export const get = async (path,success,error)=>{
-	try{
-		const res = await api.get("/Proxy?op="+path)
-		if(success!=null)
-			success(res)		
-	}catch(e){
-		if(error!=null)
-			error(e)
+};
+export const get = async (path, success, error) => {
+	try {
+		const res = await api.get('/Proxy?op=' + path);
+		if (success != null) success(res);
+	} catch (e) {
+		if (error != null) error(e);
 	}
-}
-
-
-
+};
